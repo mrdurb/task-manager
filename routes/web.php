0f91241e71894458ef5bad8f14da590e820add3e
unknown <stas.kuz26@gmail.com>
2019-12-07 23:15:20 +0700

    Add 'Employee Controller' and view 'Employee'

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('employee');
 });
+
+Route::resource('employee', 'EmployeeController');
+
+Route::post('employee/update', 'EmployeeController@update')->name('employee.update');
+
+Route::get('employee/destroy/{id}', 'EmployeeController@destroy');
