ee92b4a6173f1761a650e6c4a970bac98427d2b4
unknown <stas.kuz26@gmail.com>
2019-12-09 21:18:17 +0700

    Add modal windows

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -18,16 +18,57 @@
                 <td>{{ $employee->name }}</td>
                 <td>{{ $employee->position }}</td>
                 <td>
-                    <button class="btn btn-primary">Редактировать</button>
+                    <button class="btn btn-primary" id="editEmployee">Редактировать</button>
                     <button class="btn btn-danger">Удалить</button>
                 </td>
             </tr>
             @empty
-                <tr>
-                    <td colspan="2">Нет записей</td>
-                </tr>
+            <tr>
+                <td colspan="2">Нет записей</td>
+            </tr>
             @endforelse
         </tbody>
     </table>
 </div>
+<button class="btn btn-success float-right" id="addEmployee">Добавить</button>
+</div>
+<script>
+    var addEmployeeModal = new jBox('Modal', {
+        attach: '#addEmployee',
+        title: '<h4>Добавить исполнителя</h2>',
+        content: `  <div class="input-group input-group-lg mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Имя</span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="input-group input-group-lg mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Должность</span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                    <button class="btn btn-danger float-right" onclick="addEmployeeModal.close();">Отмена</button>
+                    <button class="btn btn-success float-right mr-1">Сохранить</button>`
+    });
+
+    var editEmployeeModal = new jBox('Modal', {
+        attach: '#editEmployee',
+        title: '<h4>Редактировать исполнителя</h2>',
+        content: `  <div class="input-group input-group-lg mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Имя</span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="input-group input-group-lg mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Должность</span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                    <button class="btn btn-danger float-right" onclick="editEmployeeModal.close();">Отмена</button>
+                    <button class="btn btn-success float-right mr-1">Сохранить</button>`
+    });
+</script>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,16 +8,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>   
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- jBox -->
+    <script src="https://cdn.jsdelivr.net/gh/StephanWagner/jBox@v1.0.5/dist/jBox.all.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/gh/StephanWagner/jBox@v1.0.5/dist/jBox.all.min.css" rel="stylesheet">
     <title>Менеджер задач</title>
 </head>
 <body>
     <div class="container">
         <ul class="nav nav-tabs mt-5">
             <li class="nav-item">
-                <a class="nav-link" href="#">Задачи</a>
+                <a class="nav-link" href="#" id="taskTable">Задачи</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" href="#">Исполнители</a>
+                <a class="nav-link active" href="#" id="employeeTable">Исполнители</a>
             </li>
         </ul>
         @yield('employees_table')
